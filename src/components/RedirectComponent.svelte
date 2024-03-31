a99cf7fd02a474125b0c79d86c3f400568ef56a6
Rawlin <rawlincrasto@gmail.com>
2024-03-31 16:19:54 +0530

    initial commit rawlincrasto.com

---
+++ src/components/RedirectComponent.svelte
@@ -0,0 +1,48 @@
+<script>
+	export let redirectName = 'Redirect';
+	export let redirectLink = 'https://www.google.com';
+
+    function handleClick() {
+        window.open(redirectLink, '_blank', 'noopener,noreferrer')
+    }
+</script>
+
+<div class="container" on:click={handleClick}>
+	<p>{redirectName}</p>
+	<svg class="icon"
+		xmlns="http://www.w3.org/2000/svg"
+		width="1em"
+		height="1em"
+		viewBox="0 0 24 24"
+		{...$$props}
+	>
+		<path
+			fill="currentColor"
+			d="m16 8.4l-8.9 8.9q-.275.275-.7.275t-.7-.275q-.275-.275-.275-.7t.275-.7L14.6 7H7q-.425 0-.712-.288T6 6q0-.425.288-.712T7 5h10q.425 0 .713.288T18 6v10q0 .425-.288.713T17 17q-.425 0-.712-.288T16 16z"
+		/>
+	</svg>
+</div>
+
+<style>
+	.container {
+		display: flex;
+		align-items: center;
+		cursor: pointer;
+	}
+
+	.icon {
+		transition: transform 0.3s ease;
+		margin-left: 4px;
+	}
+
+	.container:hover .icon {
+		transform: translate(0.2em, -0.2em);
+	}
+
+    .container.fill-div {
+        display: block;
+        height: 100%;
+        width: 100%;
+        text-decoration: none;
+    }
+</style>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,47 @@
+<script>
+    import '../global.css'
+    import Redirect from '../components/RedirectComponent.svelte'
+    import SeeMore from '../components/SeeMoreComponent.svelte'
+</script>
+
+<div class="container">
+    <p>Hey, I'm Rawlin. I specialize in crafting user interfaces, interactions, and animations for mobile and web applications.</p>
+    <div class="container-reactions">
+        <Redirect redirectName="LinkedIn" redirectLink="https://www.google.com" />
+        <Redirect redirectName="Github" redirectLink="https://www.google.com" />
+    </div>
+    <SeeMore class="see`"/>
+</div>
+
+
+<style>
+    p {
+        margin-bottom: 2em;
+    }
+    .container {
+        display: flex;
+        flex-direction: column;
+        justify-content: center;
+        align-items: center;
+        height: 100vh;
+    }
+    .container-reactions {
+        display: flex;
+        justify-content: start;
+        align-items: start;
+    }
+    .info-container {
+        display: flex;
+        flex-direction: column;
+        justify-content: top;
+        align-items: start;
+        height: 100vh;
+    }
+    .see {
+        position: absolute;
+        bottom: 0;
+        left: 50%;
+        transform: translateX(-50%);
+    }
+    
+</style>
